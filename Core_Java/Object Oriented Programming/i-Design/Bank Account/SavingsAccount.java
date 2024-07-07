5c912974e98c70b17a4b64171fa97de575705271
SIVAKUMAR VAMMI <84323820+siva12-could@users.noreply.github.com>
2024-07-07 10:54:53 +0530

    SavingsAccount.java

---
+++ Core_Java/Object Oriented Programming/i-Design/Bank Account/SavingsAccount.java
@@ -0,0 +1,22 @@
+public class SavingsAccount extends Account{
+	//fill your code here
+	String orgName;
+
+	public SavingsAccount(String accName, String accNo, String bankName, String orgName) {
+		super(accName, accNo, bankName);
+		this.orgName = orgName;
+	}
+
+	public String getOrgName() {
+		return orgName;
+	}
+
+	public void setOrgName(String orgName) {
+		this.orgName = orgName;
+	}
+	public void display() {
+		super.display();
+		System.out.println("Organisation Name:"+orgName);
+	}
+	
+}
