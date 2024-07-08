8dfeded839aea783181d266e7b69f8d70b1e49c8
SIVAKUMAR VAMMI <84323820+siva12-could@users.noreply.github.com>
2024-07-08 08:17:42 +0530

    Main.java

---
+++ Core_Java/Class Packaging/i-Analysis/Product 1/Main.java
@@ -0,0 +1,23 @@
+package com.oops.encapsulation;
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		Scanner input=new Scanner(System.in);
+		Product p=new Product();
+		ProductBO pbo=new ProductBO();
+		System.out.println("Enter the product id");
+		long n=Long.parseLong(input.nextLine());
+		p.setId(n);
+		System.out.println("Enter the product name");
+		String name=input.nextLine();
+		p.setProductName(name);
+		System.out.println("Enter the supplier name");
+		String sname=input.nextLine();
+		p.setSupplierName(sname);
+		System.out.println("Enter the product price");
+		Integer price=Integer.parseInt(input.nextLine());
+		p.setPrice(price);
+		pbo.displayProductDetails(p);
+		
+	}
+}
