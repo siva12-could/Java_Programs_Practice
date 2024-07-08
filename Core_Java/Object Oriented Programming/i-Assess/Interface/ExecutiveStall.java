cc1ca479cdedbf53ee0eca8afcf429ac08940f68
SIVAKUMAR VAMMI <84323820+siva12-could@users.noreply.github.com>
2024-07-08 06:35:45 +0530

    ExecutiveStall.java

---
+++ Core_Java/Object Oriented Programming/i-Assess/Interface/ExecutiveStall.java
@@ -0,0 +1,53 @@
+public class ExecutiveStall implements Stall {
+    private String stallName;
+    private Integer cost;
+    private String ownerName;
+    private Integer screen;
+
+    public ExecutiveStall(String stallName, Integer cost, String ownerName, Integer screen) {
+        this.stallName = stallName;
+        this.cost = cost;
+        this.ownerName = ownerName;
+        this.screen = screen;
+    }
+
+    public String getStallName() {
+        return stallName;
+    }
+
+    public void setStallName(String stallName) {
+        this.stallName = stallName;
+    }
+
+    public Integer getCost() {
+        return cost;
+    }
+
+    public void setCost(Integer cost) {
+        this.cost = cost;
+    }
+
+    public String getOwnerName() {
+        return ownerName;
+    }
+
+    public void setOwnerName(String ownerName) {
+        this.ownerName = ownerName;
+    }
+
+    public Integer getScreen() {
+        return screen;
+    }
+
+    public void setScreen(Integer screen) {
+        this.screen = screen;
+    }
+
+    @Override
+    public void display() {
+        System.out.println("Stall Name:" + stallName);
+        System.out.println("Cost:" + cost + ".Rs");
+        System.out.println("Owner Name:" + ownerName);
+        System.out.println("Number of Screens:" + screen);
+    }
+}
