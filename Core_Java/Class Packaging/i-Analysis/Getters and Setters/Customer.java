1bbc40b74544d0e6183f5cb368304d47464ea4dd
SIVAKUMAR VAMMI <84323820+siva12-could@users.noreply.github.com>
2024-07-08 07:58:17 +0530

    Customer.java

---
+++ Core_Java/Class Packaging/i-Analysis/Getters and Setters/Customer.java
@@ -0,0 +1,45 @@
+
+public class Customer {
+	Integer id;
+	String name;
+	String email;
+	String address;
+	String purpose;
+	String otherDetails;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getPurpose() {
+		return purpose;
+	}
+	public void setPurpose(String purpose) {
+		this.purpose = purpose;
+	}
+	public String getOtherDetails() {
+		return otherDetails;
+	}
+	public void setOtherDetails(String otherDetails) {
+		this.otherDetails = otherDetails;
+	}
+}
