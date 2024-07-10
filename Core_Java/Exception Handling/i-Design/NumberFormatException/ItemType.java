fa7457819bdfff8c9c255d6bb9e49e7e639c2721
SIVAKUMAR VAMMI <84323820+siva12-could@users.noreply.github.com>
2024-07-10 18:29:13 +0530

    ItemType.java

---
+++ Core_Java/Exception Handling/i-Design/NumberFormatException/ItemType.java
@@ -0,0 +1,35 @@
+public class ItemType {
+//Fill your code here	
+	String name;
+	Double deposit;
+	Double costPerDay;
+	public ItemType(String name, Double deposit, Double costPerDay) {
+		super();
+		this.name = name;
+		this.deposit = deposit;
+		this.costPerDay = costPerDay;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Double getDeposit() {
+		return deposit;
+	}
+	public void setDeposit(Double deposit) {
+		this.deposit = deposit;
+	}
+	public Double getCostPerDay() {
+		return costPerDay;
+	}
+	public void setCostPerDay(Double costPerDay) {
+		this.costPerDay = costPerDay;
+	}
+	@Override
+	public String toString() {
+		return "Name:"+name+"\nDeposit:"+deposit+"\nCost Per Day"+costPerDay;
+	}
+	
+}
