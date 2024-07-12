7b0be4ef7ca11a8b6fec6d4a30f9b0d5d29d3c72
SIVAKUMAR VAMMI <84323820+siva12-could@users.noreply.github.com>
2024-07-12 08:37:13 +0530

    DuplicateMobileNumberException.java

---
+++ Core_Java/Exception Handling/i-Design/Duplicate mobile number exception/import java.io.*; public class DuplicateMobileNumberException extends Exception{ 	/DuplicateMobileNumberException.java
@@ -0,0 +1,7 @@
+import java.io.*;
+public class DuplicateMobileNumberException extends Exception{
+	//your code here
+    public DuplicateMobileNumberException(String message) {
+    	super(message);
+    }
+}
