f9d8302f69680c1a75b196ac3de4be00431ad2cf
SIVAKUMAR VAMMI <84323820+siva12-could@users.noreply.github.com>
2024-07-12 16:39:05 +0530

    Customer.java

---
+++ Core_Java/Exception Handling/Fundamental Classes/Equals & == String/Customer.java
@@ -0,0 +1,15 @@
+public class Customer{
+	String name,email;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name=name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email=email;
+	}
+}
