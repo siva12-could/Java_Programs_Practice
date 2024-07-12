1f800e5b0dd07849a45921f4d6ba7e1d6abfc758
SIVAKUMAR VAMMI <84323820+siva12-could@users.noreply.github.com>
2024-07-12 17:22:10 +0530

    Item.java

---
+++ Core_Java/Fundamental Classess/i-Design/String Builder/Item.java
@@ -0,0 +1,32 @@
+public class Item {
+	//fill the code here
+	private String name;
+	private String type;
+	private Integer cost;
+	private Integer availableQuantity;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public Integer getCost() {
+		return cost;
+	}
+	public void setCost(Integer cost) {
+		this.cost = cost;
+	}
+	public Integer getAvailableQuantity() {
+		return availableQuantity;
+	}
+	public void setAvailableQuantity(Integer availableQuantity) {
+		this.availableQuantity = availableQuantity;
+	}
+	
+}
