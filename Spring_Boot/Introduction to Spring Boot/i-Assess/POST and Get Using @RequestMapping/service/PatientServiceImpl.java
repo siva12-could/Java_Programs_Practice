a6e754163ebdad1f733ac338fd99101989a38ec5
SIVAKUMAR VAMMI <84323820+siva12-could@users.noreply.github.com>
2024-07-13 17:50:47 +0530

    PatientServiceImpl.java

---
+++ Spring_Boot/Introduction to Spring Boot/i-Assess/POST and Get Using @RequestMapping/service/PatientServiceImpl.java
@@ -0,0 +1,24 @@
+package com.springboot.service;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.springboot.dao.PatientDAO;
+import com.springboot.domain.Patient;
+
+@Service
+public class PatientServiceImpl implements PatientService {
+
+    @Autowired
+    private PatientDAO patientDAO;
+
+    @Override
+    public List<Patient> createPatient(Patient patient) {
+        return patientDAO.createPatient(patient);
+    }
+
+    @Override
+    public List<Patient> getPatients() {
+        return patientDAO.getPatients();
+    }
+}
