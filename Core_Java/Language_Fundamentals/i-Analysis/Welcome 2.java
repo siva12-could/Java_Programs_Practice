2e24227aec37f6f723a79faaa38f506a27279560
SIVAKUMAR VAMMI <84323820+siva12-could@users.noreply.github.com>
2024-07-17 12:59:37 +0530

    Welcome 2.java

---
+++ Core_Java/Language_Fundamentals/i-Analysis/Welcome 2.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class Main{
+	public static void main(String[] args) throws Exception {
+		BufferedReader input=new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Enter the course name");
+		String name=input.readLine();
+		System.out.println("Welcome to "+name);
+		
+	}
+}
