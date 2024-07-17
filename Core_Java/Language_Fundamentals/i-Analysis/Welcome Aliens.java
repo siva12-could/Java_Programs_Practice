982cd4763b0338279f7194f22fd140f934bcb883
SIVAKUMAR VAMMI <84323820+siva12-could@users.noreply.github.com>
2024-07-17 12:57:18 +0530

    Welcome Aliens.java

---
+++ Core_Java/Language_Fundamentals/i-Analysis/Welcome Aliens.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class Main{
+	public static void main(String[] args) throws Exception {
+		BufferedReader input=new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Enter your name");
+		String name=input.readLine();
+		System.out.println("Hello "+name+" ! Welcome to our planet Earth.");
+		
+	}
+}
