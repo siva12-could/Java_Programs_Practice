e058930363ef3a7a0652a5b7358c465bba2f7074
SIVAKUMAR VAMMI <84323820+siva12-could@users.noreply.github.com>
2024-07-22 09:03:28 +0530

    MovieCatalogSystemSpringBootJpaApplication.java

---
+++ Java_Additional_Assignments/Spring and Spring Boot/Spring Data JPA program facilitates the management/MovieCatalogSystemSpringBootJpaApplication.java
@@ -0,0 +1,14 @@
+package com.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//Fill your code here
+@SpringBootApplication
+public class MovieCatalogSystemSpringBootJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieCatalogSystemSpringBootJpaApplication.class, args);
+	}
+
+}
