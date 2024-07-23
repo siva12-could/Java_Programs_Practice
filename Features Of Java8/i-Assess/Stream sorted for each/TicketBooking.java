87dafe31a0fd18392451a7d73eaa6337fccc1608
SIVAKUMAR VAMMI <84323820+siva12-could@users.noreply.github.com>
2024-07-23 19:37:22 +0530

    TicketBooking.java

---
+++ Features Of Java8/i-Assess/Stream sorted for each/TicketBooking.java
@@ -0,0 +1,31 @@
+import java.util.List;
+
+class TicketBooking{
+    private String customerName;
+    private Integer noOfTickets;
+    private Double price;
+    
+    TicketBooking(String customerName, Integer noOfTickets, Double price){
+        //Fill code here
+    	this.customerName=customerName;
+    	this.noOfTickets=noOfTickets;
+    	this.price=price;
+    }
+    String getCustomerName(){
+        return this.customerName;
+    }
+    Integer getNoOfTickets(){
+        return this.noOfTickets;
+    }
+    Double getPrice(){
+        return this.price;
+    }
+    public String toString() {
+        
+		//Fill code here
+    	
+    	return String.format("%-10s %-15s %-15s",customerName,noOfTickets,price);
+    	//return 
+    	
+    } 
+}
