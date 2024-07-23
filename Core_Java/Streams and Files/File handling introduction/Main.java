d60effecd10f4198c1416f214b43ffcffcf2a33b
SIVAKUMAR VAMMI <84323820+siva12-could@users.noreply.github.com>
2024-07-23 23:41:14 +0530

    Main.java

---
+++ Core_Java/Streams and Files/File handling introduction/Main.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        try (BufferedReader br = new BufferedReader(new FileReader("input.csv"))) {
+            UserBO userBO = new UserBO();
+            List<User> userList = userBO.readFromFile(br);
+            userBO.display(userList);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
