1466a8015b55badac283e00816049c4941981a75
SIVAKUMAR VAMMI <84323820+siva12-could@users.noreply.github.com>
2024-08-03 12:21:28 +0530

    HotelRoom.java

---
+++ Java-Additional Assignments/Java/Abstract Class - Hotel/HotelRoom.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public abstract class HotelRoom {
+    protected String hotelName;
+    protected int numberOfSqFeet;
+
+    public HotelRoom() {}
+
+    public HotelRoom(String hotelName, int numberOfSqFeet) {
+        this.hotelName = hotelName;
+        this.numberOfSqFeet = numberOfSqFeet;
+    }
+
+    public abstract int calculateTariff();
+
+    public String getHotelName() {
+        return hotelName;
+    }
+
+    public void setHotelName(String hotelName) {
+        this.hotelName = hotelName;
+    }
+
+    public int getNumberOfSqFeet() {
+        return numberOfSqFeet;
+    }
+
+    public void setNumberOfSqFeet(int numberOfSqFeet) {
+        this.numberOfSqFeet = numberOfSqFeet;
+    }
+}
