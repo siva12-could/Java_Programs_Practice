6cfbe8d19a03d327ad17833b2216ad701fbebd93
SIVAKUMAR VAMMI <84323820+siva12-could@users.noreply.github.com>
2024-08-07 00:22:13 +0530

    User.java

---
+++ Core_Java/MultiThreading/City Count Approach_1/User.java
@@ -0,0 +1,55 @@
+class User {
+    private String name;
+    private String mobileNumber;
+    private String city;
+    private String state;
+
+
+    public User() {
+        this.name = "";
+        this.mobileNumber = "";
+        this.city = "";
+        this.state = "";
+    }
+
+  
+    public User(String name, String mobileNumber, String city, String state) {
+        this.name = name;
+        this.mobileNumber = mobileNumber;
+        this.city = city;
+        this.state = state;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
+
+    public void setMobileNumber(String mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+}
